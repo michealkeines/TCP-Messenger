e4d64246d7c5b1ccda0b7fd35ef50b754dfaf452
micheal keines <keinesmicheal@gmail.com>
2022-06-20 10:14:03 -0400

    added all files

---
+++ src/receiver.rs
@@ -0,0 +1,25 @@
+use std::time::Instant;
+use crate::message::Message;
+
+pub struct ReceiveMessage<T> {
+    data: Message<T>
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn is_message() {
+        let message = 12;
+        let time = Instant::now();
+        let m = Message::new(message, time);
+
+        let recv_data = ReceiveMessage {
+            data: m
+        };
+
+        assert_eq!(*recv_data.data.message.as_ref(),12);
+    }
+}
+
